3f54e8ec395ceef505bd750ddb3daa536ec3d481
matuyama <syumatu1997@gmail.com>
2023-10-03 11:07:24 +0900

    [add]応用課題js-2

--- Applied.js/add.js
+++ Applied.js/add.js
@@ -1,1 +1,32 @@
-alert('Hello Word')
+let hello = ('Hello'+'World');
+alert (hello);
+
+let orang = 100;
+let apple = 120;
+
+if (orang < apple) {
+    alert('みかんの値段がリンゴよりも安い');
+}else if(orang == apple){
+    alert('みかんとリンゴが同じ値段');
+}else{
+    alert('みかんの値段がリンゴより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while (num < max) {
+    num = num*2;
+    count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let num1 = 0;
+for(i = 1; i < 11; i ++){
+    num1 = num1 + i;
+}
+
+alert('1から10まで足し算した結果は' + num1 + 'です');
