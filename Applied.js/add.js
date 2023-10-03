7400e51bf124a7f4eac3b3184729dd3ba1fefe1d
matuyama <syumatu1997@gmail.com>
2023-10-03 23:15:57 +0900

    [add]応用課題js-3

--- Applied.js/add.js
+++ Applied.js/add.js
@@ -1,32 +1,64 @@
-let hello = ('Hello'+'World');
-alert (hello);
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") &&(user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)) {
+    alert("グー・チョキ・パーのいずれかを入力してください");
+    user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+}
 
-let orang = 100;
-let apple = 120;
+let js_hand = getJShand();
 
-if (orang < apple) {
-    alert('みかんの値段がリンゴよりも安い');
-}else if(orang == apple){
-    alert('みかんとリンゴが同じ値段');
+let judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+    alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
 }else{
-    alert('みかんの値段がリンゴより高い');
+    alert("またチャレンジしてね")
 }
 
-let max = 100;
-let num = 1;
-let count = 0;
 
-while (num < max) {
-    num = num*2;
-    count = count + 1;
+
+function getJShand() {
+    let js_hand_num = Math.floor( Math.random()*3 );
+    let hand_name;
+
+    if (js_hand_num == 0){
+        hand_name = "グー";
+    }else if (js_hand_num == 1){
+        hand_name = "チョキ";
+    }else if (js_hand_num == 2){
+        hand_name = "パー";
+    }
+    return hand_name;
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+function winLose(user, js) {    
+    let winLoseStr;
 
-let i;
-let num1 = 0;
-for(i = 1; i < 11; i ++){
-    num1 = num1 + i;
+    if(user == "グー"){
+        if(js == "グー"){
+            winLoseStr = "あいこ";
+        }
+        else if(js == "チョキ"){
+            winLoseStr = "勝ち";
+        }
+        else if(js == "パー"){
+            winLoseStr = "負け";
+        }
+    }else if(user == "チョキ"){
+        if(js == "グー"){
+            winLoseStr = "負け";
+        }else if(js == "チョキ"){
+            winLoseStr = "あいこ";
+        }else if(js == "パー"){
+            winLoseStr = "勝ち";
+        }
+    }else if(user == "パー"){
+        if(js = "グー"){
+            winLoseStr = "勝ち";
+        }else if(js = "チョキ"){
+            winLoseStr = "負け";            
+        }else if(js = "パー"){
+            winLoseStr = "あいこ";
+        }
+    }
+    return winLoseStr;
 }
-
-alert('1から10まで足し算した結果は' + num1 + 'です');
